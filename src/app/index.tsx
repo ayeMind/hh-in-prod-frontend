3965deb97a2944861e22bd01ea32bbd1c260b3de
Yan <fzerix@gmail.com>
2024-03-31 07:44:56 +0300

    add alias

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { Providers } from './providers';
-import { router } from "./router";
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
