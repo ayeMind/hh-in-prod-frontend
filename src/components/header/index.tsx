99feb28a4a84c81c56e983e52323a4fb0cf29fd2
zotovy <the1ime@yandex.ru>
2024-03-31 17:51:52 +0300

    feat(ui): create mobile header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,38 +1,57 @@
-import { Container, Group, Text, Flex, Divider } from "@mantine/core";
-import { Link } from "react-router-dom";
+import { Container, Group, Text, Flex, Divider, Burger, Drawer } from "@mantine/core"
+import { Link } from "react-router-dom"
 import classes from "./styles.module.css"
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 
 const links = [
-    { link: '/hackathon/1/teams', label: 'Команды' },
-    { link: '/resume', label: 'Мое резюме' },
-    { link: '/profile', label: 'Профиль' },
+    {link: '/hackathon/1/teams', label: 'Команды'},
+    {link: '/resume', label: 'Мое резюме'},
+    {link: '/profile', label: 'Профиль'},
 ];
 
 
 export const Header = () => {
+    const isMobile = useMediaQuery('(max-width: 500px)')
+    const [opened, {toggle, close}] = useDisclosure();
 
     const items = links.map((link) => (
-        <Link className={classes["link"]}
-          data-active={window.location.pathname === link.link || undefined}
-          key={link.label}
-          to={link.link}
-
-        >
-          {link.label}
+        <Link
+            className={ classes["link"] }
+            data-active={ window.location.pathname === link.link || undefined }
+            key={ link.label }
+            to={ link.link }>
+            { link.label }
         </Link>
     ));
 
-  return (
-    <header className={classes["header"]}>
-        <Container size="md">
-          <Flex justify={"space-between"} align={"center"} p="md">
-            <Text size="md" fw={"600"}>LOGO</Text>
-            <Group gap="md" >
-              {items}
-            </Group>
-          </Flex>
-            <Divider w={"100%"} mb="xl" />
-        </Container>
-    </header>
-  );
+    return (
+        <header className={ classes["header"] }>
+            <Container size="md">
+                <Flex justify={ "space-between" } align={ "center" } py="md">
+                    <Text size="md" fw={ "600" }>LOGO</Text>
+                    {
+                        isMobile
+                            ? <Burger
+                                onClick={ toggle }
+                                opened={ opened }/>
+                            : <Group gap="md">
+                                { items }
+                            </Group>
+                    }
+                    {
+                        isMobile && <Drawer
+                            opened={ opened }
+                            onClose={ close }
+                            size='100%'
+                            title={ <Text size="md" fw={ "600" }>LOGO</Text> }>
+                            <Flex gap="md" direction={"column"}>
+                                { items }
+                            </Flex>
+                        </Drawer>
+                    }
+                </Flex>
+                <Divider w={ "100%" } mb="xl"/>
+            </Container>
+        </header>
+    );
 };
--- src/pages/teams/org/index.tsx
+++ src/pages/teams/org/index.tsx
@@ -1,5 +1,5 @@
 import { FC, memo } from "react";
-import { Button, Container, Flex, SimpleGrid, Text } from "@mantine/core";
+import { Button, Container, Flex, SimpleGrid } from "@mantine/core";
 import { Header } from "@/components/header";
 import { IconPlus } from "@tabler/icons-react";
 import { CurrentTeamCard } from "@/components/current-team-card";
@@ -17,8 +17,8 @@ export const TeamOrgPage: FC<TeamOrgPageProps> = memo(() => {
         <Header/>
         <Container size="md" mt="md">
             {/*  Head */ }
-            <Flex justify="space-between" mb='md'>
-                <Text size="xl">Команды</Text>
+            <Flex justify="space-between" mb='md' align='center'>
+                <h1>Команды</h1>
                 <Button
                     variant="outline"
                     rightSection={ <IconPlus size={ 14 }/> }>
