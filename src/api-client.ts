905dfb9bf1786312845378b8ab549569b0ddbb46
zotovy <the1ime@yandex.ru>
2024-03-31 21:27:27 +0300

    feat(auth): implement signup & login

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: import.meta.env.VITE_API_CLIENT,
     validateStatus: (status) => status != 500,
 })
 
