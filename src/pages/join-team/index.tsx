ac0d2604f149691710936cfe6ae13c72b526fbfc
Yan <fzerix@gmail.com>
2024-04-02 02:29:09 +0300

    add join team

---
+++ src/pages/join-team/index.tsx
@@ -0,0 +1,64 @@
+import { Button, Center, Flex, Text } from "@mantine/core";
+import styles from './join-team.module.css';
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import parseJwt from "@/utils/parse-jwt.ts";
+import { AuthGuard } from "@/components/auth-guard";
+import joinTeamById from "@/api/join-team-by-id";
+import getTeam from "@/api/get-team";
+import { ITeam } from "@/models/ITeam";
+
+export const JoinTeam = () => {
+    const [searchParams] = useSearchParams();
+    const [team, setTeam] = useState<ITeam | null>()
+    const [loading, setLoading] = useState(true)
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const id = parseInt(parseJwt(searchParams.get("team_id") as string, 'id') ?? '')
+        
+        if (!id) {
+            navigate('/')
+        } else {
+            getTeam(id)
+                .then(data => {
+                    setTeam(data);
+                    setLoading(false);
+                })
+        }
+    }, [])
+
+    const onClick = async () => {
+        if (loading || !team) return
+        setLoading(true)
+
+        const success = await joinTeamById(team.id)
+        if (success) {
+            navigate(`/team/${ team.id }/create-resume`)
+        } else {
+            console.error('error')
+        }
+    }
+
+    if (!team) {
+        return <AuthGuard role='user'/>
+    }
+
+
+    return (
+        <AuthGuard role='user'>
+            <Flex component={ Center } h={ "100vh" } direction={ "column" }>
+                <Text fw="500" size={ "xl" } mb={ "sm" } className={ styles.title }>
+                    Привет!
+                    <br/>
+                    Тебя пригласили в команду «{ team.name }»
+                </Text>
+                <Button
+                    loading={ loading }
+                    onClick={ onClick }>
+                    Принять приглашение
+                </Button>
+            </Flex>
+        </AuthGuard>
+    )
+}
