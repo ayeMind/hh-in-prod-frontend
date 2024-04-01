df8c0624d122a62609062beb2a5d4b652852cda8
zotovy <the1ime@yandex.ru>
2024-04-01 18:58:43 +0300

    feat(models): change ITeam

--- src/models/ITeam.ts
+++ src/models/ITeam.ts
@@ -1,6 +1,7 @@
+import { IUser } from "@/models/IUser.ts";
+
 export interface ITeam {
-    id: number;
-    name: string;
-    teamLeaderUserId: number;
-    vacanciesIds: number;
+    id: number
+    name: string
+    members: IUser[]
 }
