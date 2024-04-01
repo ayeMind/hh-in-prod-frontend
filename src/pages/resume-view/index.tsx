53997be56ff7201c5c79856c36b91cdda7499c5f
Yan <fzerix@gmail.com>
2024-04-02 01:27:56 +0300

    connect with backend resume view

--- src/pages/resume-view/index.tsx
+++ src/pages/resume-view/index.tsx
@@ -1,23 +1,41 @@
-import { Avatar, Container, Flex, Text, Badge } from "@mantine/core"
+import { Container, Text, Badge } from "@mantine/core"
 import { Header } from "@/components/header";
 import { AuthGuard } from "@/components/auth-guard";
+import { useEffect, useState } from "react";
+import { fetchResume } from "@/api/fetch-resume";
+import { IResume } from "@/models/IResume";
+import { useParams } from "react-router-dom";
 
-const contacts = ["@petya123", "www.github.com/petya_cool", "petya@gmail.com"]
-const techSkills = ["Python", "JavaScript", "Git", "React", "Angular"]
-const softSkills = ["Ответственный", "Крутой", "Очень смешной", "Удивительный", "Гениальный", "Сверхразум", "Диктор", "Манимулятор", "Удивительный", "Ответственный", "Крутой",]
+
 
 export const ResumeView = () => {
+
+    const [resume, setResume] = useState<IResume | null>(null)
+    const [contacts, setContacts] = useState<string[]>([])
+
+    const { hackathon_id, user_id } = useParams()
+
+    useEffect(() => {
+        fetchResume(parseInt(user_id as string), parseInt(hackathon_id as string)).then(data => {
+            setResume(data)
+            if (data?.telegram) setContacts(prev => [...prev, data.telegram as string ])
+            if (data?.hhLink) setContacts(prev => [...prev, data.hhLink as string])
+            if (data?.personalWebsite) setContacts(prev => [...prev, data.personalWebsite as string])
+            return;
+        })
+    }, [])
+
     const contactsItems = contacts.map(contact => (
         <Text mb="md">{contact}</Text>
     ))
 
-    const techSkillsItems = techSkills.map(skill => (
+    const techSkillsItems = resume?.techStack.map(skill => (
         <Badge p="sm" mr="xs" mb="xs">
             <p>{skill}</p>
         </Badge>
     ))
 
-    const softSkillsItems = softSkills.map(skill => (
+    const softSkillsItems = resume?.softSkills.map(skill => (
         <Badge p="sm" mr="xs" mb="xs">
             <p>{skill}</p>
         </Badge>
@@ -27,34 +45,36 @@ export const ResumeView = () => {
     <AuthGuard role='any'>
        <Header variant="user" />
        <Container>
-        <Flex align="center" gap="md">
-            <Avatar w={100} h={100} />
-            <h2>Петя Петров</h2>
-        </Flex>
-
-        <Container mt="xl">
+ 
+        {resume?.bio && <Container mt="xl">
             <h3>Обо мне</h3>
-            <Text>Я настолько крутой, что мне даже не нужно ничего тут писать, но тут можно типо дофига текста о себе написать</Text>
-        </Container>
+            <Text>
+                {resume?.bio}
+            </Text>
+        </Container>}
 
-        <Container mt="xl">
+        {contacts.length > 0 && <Container mt="xl">
             <h3>Контакты</h3>
             <Text>{contactsItems}</Text>
-        </Container>
+        </Container>}
 
-        <Container mt="xl">
+        {resume?.techStack && <Container mt="xl">
             <h3>Tech Skills</h3>
             <Container mt="sm" pl={0}>
                 {techSkillsItems}
             </Container>
-        </Container>
+        </Container>}
 
-        <Container mt="xl">
+        {resume?.softSkills && <Container mt="xl">
             <h3>Soft Skills</h3>
             <Container mt="sm" pl={0}>
                 {softSkillsItems}
             </Container>
-        </Container>
+        </Container>}
+
+        {!resume?.softSkills && !resume?.bio && contacts.length === 0 && !resume?.techStack && (
+            <Text>У пользователя пустое резюме</Text>
+        )}
 
        </Container>
     </AuthGuard>
