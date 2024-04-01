a235ce18a55e3c8b1d7aff60e6c0ef08dda59a6e
Vadimcha <BelovBadim2014@yandex.ru>
2024-04-01 19:31:58 +0300

    Make getting team-members data

--- src/components/team-detail-participants/index.tsx
+++ src/components/team-detail-participants/index.tsx
@@ -2,14 +2,16 @@ import {MemberCard} from "@/components/member-card";
 import {Button, SimpleGrid} from "@mantine/core";
 import {TeamInvitePopup} from "@/components/team-invite-popup";
 import {useDisclosure} from "@mantine/hooks";
+import {IUser} from "@/models/IUser";
 
-export const TeamDetailParticipants = () => {
+export const TeamDetailParticipants = ({ members }: { members: IUser[] }) => {
     const [opened, {close, open}] = useDisclosure(false)
     return (
         <>
             <SimpleGrid cols={ {base: 1, xs: 2, sm: 3} } spacing="md" mt={ 12 } mb={ 36 }>
-                <MemberCard name='Иван Иванов' email='vanya@gmail.com'/>
-                <MemberCard name='Петя Петров' email='petya@gmail.com'/>
+                { members.map((member: IUser) => {
+                    return <MemberCard key={member.id} name={member.name} email={member.email} />
+                }) }
                 <Button variant='outline' h="100%" mih='50px' onClick={ open }>Добавить</Button>
             </SimpleGrid>
 
--- src/pages/team-detail/index.tsx
+++ src/pages/team-detail/index.tsx
@@ -1,16 +1,33 @@
-import { FC, memo } from "react";
+import {memo, useEffect, useState} from "react";
 import { AuthGuard } from "@/components/auth-guard";
 import { Header } from "@/components/header";
-import { Button, Container, Flex } from "@mantine/core";
+import {Button, Center, Container, Flex, Loader} from "@mantine/core";
 import {TeamDetailParticipants} from "@/components/team-detail-participants";
 import {TeamDetailVacancies} from "@/components/team-detail-vacancies";
 import {TeamDetailVacanciesResponses} from "@/components/team-detali-vacancies-responses";
 import useUser from "@/hooks/use-user";
+import {useNavigate, useParams} from "react-router-dom";
+import {ITeam} from "@/models/ITeam";
+import getTeam from "@/api/get-team";
 
-export type TeamDetailPageProps = {}
-
-export const TeamDetailPage: FC<TeamDetailPageProps> = memo(() => {
+export const TeamDetailPage = memo(() => {
     const { user } = useUser()
+    const params = useParams()
+    const [teamDetail, setTeamDetail] = useState<ITeam | null>(null)
+    const navigate = useNavigate()
+    useEffect(() => {
+        const id = parseInt(params.team_id ?? '')
+        if (id) {
+            getTeam(id).then(setTeamDetail)
+        } else {
+            navigate('/404')
+        }
+    }, [])
+    if(!teamDetail) {
+        return <Center w='100vw' h='100vh'>
+            <Loader size="md"/>
+        </Center>
+    }
     return <AuthGuard role='any'>
         <>
             <Header variant={user?.role ? user?.role : 'default'}/>
@@ -22,14 +39,14 @@ export const TeamDetailPage: FC<TeamDetailPageProps> = memo(() => {
                     align={ {base: 'flex-start', sm: 'center'} }
                     mb='50'
                     direction={ {base: 'column', sm: 'row'} }>
-                    <h1>Название команды</h1>
+                    <h1>{teamDetail.name}</h1>
 
                     <Button variant='transparent' px={ 0 }>Редактировать</Button>
                 </Flex>
 
                 {/*  Участники + Popup   */ }
                 <h3>Участники команды</h3>
-                <TeamDetailParticipants />
+                <TeamDetailParticipants members={teamDetail.members} />
 
                 {/* Вакансии */ }
                 <h3>Вакансии</h3>
