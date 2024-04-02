c15781da454300c23a2d0e3cce8583efbd780d04
Yan <fzerix@gmail.com>
2024-04-02 10:23:18 +0300

    add stop propagation

--- src/components/member-card/index.tsx
+++ src/components/member-card/index.tsx
@@ -26,7 +26,8 @@ export const MemberCard: FC<MemberCardProps> = memo(props => {
             </div>
         </div>
         {user && (props.email !== user.email) && props.creator && props.team_id && props.creator === user.id && (
-            <ActionIcon onClick={() => {
+            <ActionIcon onClick={(e) => {
+                e.stopPropagation()
                 deleteParticipant(props.team_id as number, props.email).then(()=>{
                     window.location.reload()
                 })
