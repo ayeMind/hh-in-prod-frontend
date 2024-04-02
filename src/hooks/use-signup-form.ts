d7fd6519f00c1e830654dbfca83fe7eb7a908fe1
zotovy <the1ime@yandex.ru>
2024-04-02 14:54:42 +0300

    fix: email validation

--- src/hooks/use-signup-form.ts
+++ src/hooks/use-signup-form.ts
@@ -6,8 +6,12 @@ import login from "@/api/login.ts";
 import parseJwt from "@/utils/parse-jwt.ts";
 
 const validationSchema = yup.object({
-    name: yup.string().required('Введите имя').max(30, 'Имя слишком длинное'),
-    email: yup.string().required('Введите email').email('Неверный email').max(30, 'Email слишком длинный'),
+    name: yup.string().required('Введите имя').max(60, 'Имя слишком длинное'),
+    email: yup
+        .string()
+        .required('Введите email')
+        .matches(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Неверный email')
+        .max(30, 'Email слишком длинный'),
     password: yup.string().required('Введите пароль').min(6, 'Пароль слишком простой').max(100, 'Пароль слишком длинный'),
     passwordConfirm: yup.string().required('Подтвердите пароль').oneOf([yup.ref('password')], 'Пароли должны совпадать')
 })
